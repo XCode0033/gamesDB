d6fa942185ae201062720394ed7da4997d35c35e
XCode0033 <XCode0033@gmail.com>
2026-07-12 00:59:03 -0400

    made some changes to the database. added extra tables and junction tables. Also updated MD file.

--- SQL/createTable.sql
+++ SQL/createTable.sql
@@ -1,9 +1,46 @@
 
-
-CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    gamerTag VARCHAR(100) DEFAULT 'UNNAMED',
+    age INT,
+    email VARCHAR(100) DEFAULT 'NO EMAIL'
+)
+CREATE TABLE game (
+    game_id SERIAL PRIMARY KEY,
     title  VARCHAR(100) DEFAULT 'UNTITLED',
     genre  VARCHAR(30) DEFAULT 'NONE',
     rating  INT CHECK (rating <= 5),
     description  TEXT
+
+)
+
+
+CREATE TABLE console (
+    console_id SERIAL PRIMARY KEY,
+    name VARCHAR(20),
+    generation VARCHAR(10)
+)
+
+
+
+-- Juncitons
+-- 1 user many consoles, 1 console many users
+CREATE TABLE usersAndConsole (
+    user_id INT REFERENCES user(user_id) ON DELETE CASCADE,
+    console_id INT REFERENCES console(console_id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, console_id) 
+)
+
+-- 1 user many games, 1 game many users
+CREATE TABLE usersAndGame (
+    user_id INT REFERENCES user(user_id),
+    game_id INT REFERENCES game(game_id),
+    PRIMARY KEY (user_id, game_id)
+)
+
+-- 1 game on many consoles, 1 console has many games.
+CREATE TABLE gameAndConsole (
+    game_id INT REFERENCES game(game_id),
+    console_id INT REFERENCES console(console_id),
+    PRIMARY KEY (game_id, console_id)
 )
--- documentation.md
+++ documentation.md
@@ -30,3 +30,17 @@ INSERT INTO games (title, rating, description) VALUES
 INSERT INTO games (title, genre, rating, description) VALUES
 ('Minecraft', 'Sandbox/Survival', 5, 'A sandbox survival game about mining, crafting, and building in a blocky world. It supports exploration, creativity, and survival mechanics.');
 ----------
+Sun 7/12/26 12:49 AM
+created user and console tables, also created junction tables for:
+users <-> game 
+game <-> console
+console <-> users
+
+*DID NOT* run yet.
+
+
+*TASKS FOR 6/12/26 *
+1. Run new tables.
+2. Double check data and add new insert intos.
+3. Work on incorporating joins
+*NOTE* If im not using tags on games, does the join process as it relates to tags differ?
